a3d59faca301e32e61df09d34873a93efae66a5e
Samir Katakamsetty <katakamsettysamir@gmail.com>
2026-02-13 21:01:52 -0500

    Writing logic for the polaroid component

---
+++ src/lib/components/Polaroid.svelte
@@ -0,0 +1,28 @@
+<!-- Polaroid component to show her beaautiful face-->
+
+<script lang=="ts">
+    // dont change these. actually, why are you even here?
+    interface Polaroid
+    {
+        photo: String;
+        caption: String;
+        backText: String; 
+    }
+
+    let {photo, caption, backText}: Polaroid = $props(); 
+
+    // Flipping the polaroid around
+    let isFlipped = $state(false);
+    function toggleFlip() 
+    {
+        isFlipped = !isFlipped;
+    }
+
+</script>
+
+<div>
+
+
+
+    
+</div>
